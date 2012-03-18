339b184b8409076a1eb665ee3a75e9f637fefec7
Harrison Harnisch <hharnisc@gmail.com>
2012-03-17 19:27:39 -0500

    Basic user interface elements in place on main page. Added classes to handle votes and owned.

--- main.py
+++ main.py
@@ -40,6 +40,9 @@ class AddNew(webapp.RequestHandler):
         self.response.out.write(template.render(path,template_values))
 
     def post(self):
+        #self.request.remote_addr()
+        #voted = self.request.cookies.get('voted', '')
+
         gamename=self.request.get('gamename')
         validGame = True
 
@@ -59,12 +62,19 @@ class AddNew(webapp.RequestHandler):
         #add game to database
         if validGame:
             logging.info('Adding Game Name: ' + str(gamename))
-            game = Game(title=gamename,owned=True)
+            game = Game(title=gamename,owned=False)
             vote = Vote()
             vote.put()
             game.votes.append(vote.key())
             game.put()
             template_values = {'info_message':'Successfully Added ' + str(gamename)}
+
+        #TODO: add cookie to header
+
+        #self.response.headers.add_header(
+        #    'Set-Cookie',
+        #    'username=%s; expires=Fri, 31-Dec-2020 23:59:59 GMT'\
+        #    % username.encode())
 
         path = os.path.join(os.path.dirname(__file__), 'templates'+os.sep+'addnew.html')
         self.response.out.write(template.render(path,template_values))
@@ -76,22 +86,42 @@ class GetGameData(webapp.RequestHandler):
     def get(self):
         #TODO: Use paging method to grab all items
 
-        #get all games that are not owned sorted by number of votes
+        #get all games that are not owned sorted by number of votes --cutoff at 50 characters
         results = Game.all().filter("owned",False).fetch(1000)
-        needDict = {}
+        needList = []
         for result in sorted(results,lambda a,b:cmp(len(b.votes),len(a.votes))):
-            needDict[result.title] = len(result.votes)
+            needList.append({"game":result.title[:50],"votes":len(result.votes)})
 
-        #get all games that are owned sorted alphabetically
+        #get all games that are owned sorted alphabetically -- cutoff at 50 characters
         results = Game.all().filter("owned",True).order("title").fetch(1000)
         ownedList = []
         for result in results:
-            ownedList.append(result.title)
+            ownedList.append(result.title[:50])
 
-        self.response.out.write(json.dumps({"owned":ownedList,"need":needDict}))
+        self.response.out.write(json.dumps({"owned":ownedList,"need":needList}))
+
+class VoteGame(webapp.RequestHandler):
+    """
+    Vote for a game
+    """
+    #voted = self.request.cookies.get('voted', ''
+    def get(self):
+        gamename=self.request.get('gamename')
+        result = Game.all().filter("title",gamename).fetch(1)[0]
+        #logging.info(result[0].votes)
+        vote = Vote().put()
+        result.votes.append(vote)
+        result.put()
+
+
+class SetGameOwned(webapp.RequestHandler):
+    """
+    Set a game as owned
+    """
 
 app = webapp.WSGIApplication([('/', MainHandler),
                                 ('/addnew',AddNew),
-                                ('/getgamedata',GetGameData)],
+                                ('/getgamedata',GetGameData),
+                                ('/votegame',VoteGame)],
                              debug=True)
 
